574815b47b350db077917f6ce0c1500267eaab56
Eugene <Oleksienko2003@gmail.com>
2023-01-24 20:52:10 +0200

    javascript leasson2

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// let userName = prompt('Як до вас звертатися?');
+// let age = +prompt('скільки вам років');
+// let rez;
+
+
+
+
+// if (age < 0 || age > 100 ) {
+//     rez = ('введенно некоректний вік ' + userName);
+// } 
+
+// else if(age >= 18) {
+//     rez = ("привіт " + userName)
+// }
+ 
+// else{
+// rez = ('Сюди не можна! ще малий!');
+// }
+
+// console.log(rez);
+
+
+// const month = +prompt( 'Введіть номер місяця ');
+// let rez;
+
+// switch (month) {
+//     case 1: rez = 'січень'; break;
+//     case 2: rez = 'лютий'; break;
+//     case 3: rez = 'березень'; break;
+//     case 4: rez = 'квітень'; break;
+//     case 5: rez = 'травень'; break;
+//     case 6: rez = 'червень'; break;
+
+//     default:rez = 'введенно неправельне число '; break;
+// }
+
+// console.log(rez);
+
+
+// let a = 5;
+// console.log(a);
+// a *= 2;
+// console.log(a);
+
+let a = +prompt('будь-яке число');
+let rez;
+
+// if (a > 0) { rez = 'більше нуля ';    
+// } else {rez = 'менше нуля';}
+
+(a>0) ? rez = 'bilshe nulya' : rez = 'Menshe nulya';
+console.log(rez);
